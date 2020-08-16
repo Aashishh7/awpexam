53aeb87c1503460daa88dcee70407b1ea7059be3
Aashishh7 <40740903+Aashishh7@users.noreply.github.com>
2020-08-16 12:29:17 +0530

    examanswers.

---
+++ 200240520023/2.js
@@ -0,0 +1,11 @@
+var delcount=1;
+ function a(){
+     let countme=document.createElement("div");
+     newElement.textContent="form submitted" + delcount;
+     
+     let commentbox=document.querySelector("commentbox");
+
+     commentbox.appendChild(newElement);
+     
+
+ }
